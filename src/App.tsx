77a6bd3e0d32a3bba322230c5c5d5b8b018e5aeb
dhanikprakash <dhanikprakash@gmail.com>
2021-11-01 21:21:21 +0000

    react custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import useWindowSize from './custom-hooks/WindowSize';
 
 function App() {
+  const windowSize = useWindowSize();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     Hello World!...
+     {windowSize &&(
+       <div>{JSON.stringify(windowSize)}</div>
+     )}
     </div>
   );
 }
